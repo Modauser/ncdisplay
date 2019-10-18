a5e41c3243b6159f90d82d76ca873f3e054e060b
Clyne Sullivan <clyne@bitgloo.com>
2019-10-18 11:13:04 -0400

    added esp32 .ino

---
+++ esp32/esp32.ino
@@ -0,0 +1,183 @@
+// Load Wi-Fi library
+#include <WiFi.h>
+#include <driver/spi_slave.h>
+
+#define SPI_MOSI 13
+#define SPI_MISO 12
+#define SPI_SCK  14
+#define SPI_CS   15
+#define SPI_HS   2
+
+class SPIClass
+{
+private:
+  static void postSetupCallback(spi_slave_transaction_t *trans);
+  static void postTransferCallback(spi_slave_transaction_t *trans);
+  
+  static const spi_bus_config_t config;
+  static const spi_slave_interface_config_t slvcfg;
+
+  static char txBuffer[128];
+  static char rxBuffer[128];
+  
+public:
+  void begin(void)
+  {
+    pinMode(SPI_HS, OUTPUT);
+    digitalWrite(SPI_HS, HIGH);
+    auto error = spi_slave_initialize(HSPI_HOST, &config, &slvcfg, 1);
+    if (error != ESP_OK)
+      while (1);
+    gpio_set_pull_mode((gpio_num_t)SPI_CS, GPIO_FLOATING);
+    gpio_set_pull_mode((gpio_num_t)SPI_MOSI, GPIO_FLOATING);
+    gpio_set_pull_mode((gpio_num_t)SPI_MISO, GPIO_FLOATING);
+    gpio_set_pull_mode((gpio_num_t)SPI_SCK, GPIO_FLOATING);
+  }
+
+  void end(void)
+  {
+    spi_slave_free(HSPI_HOST);
+  }
+
+  const char *transfer(const char *tx, size_t size)
+  {
+    if (size > 128)
+      size = 128;
+    spi_slave_transaction_t trans;
+    memset(&trans, 0, sizeof(trans));
+    memset(rxBuffer, 0, 128);
+    memset(txBuffer, 0, 128);
+    strncpy(txBuffer, tx, size);
+    trans.length = size * 8;
+    trans.tx_buffer = txBuffer;
+    trans.rx_buffer = rxBuffer;
+
+    spi_slave_transmit(HSPI_HOST, &trans, portMAX_DELAY);
+    return rxBuffer;
+  }
+};
+
+const spi_bus_config_t SPIClass::config = {
+  .mosi_io_num = SPI_MOSI,
+  .miso_io_num = SPI_MISO,
+  .sclk_io_num = SPI_SCK
+};
+const spi_slave_interface_config_t SPIClass::slvcfg = {
+  .spics_io_num = SPI_CS,
+  .flags = 0,
+  .queue_size = 3,
+  .mode = SPI_MODE3,
+  .post_setup_cb = postSetupCallback,
+  .post_trans_cb = postTransferCallback
+};
+char SPIClass::txBuffer[128];
+char SPIClass::rxBuffer[128];
+void SPIClass::postSetupCallback(spi_slave_transaction_t *trans)
+{
+  digitalWrite(SPI_HS, LOW);
+}
+void SPIClass::postTransferCallback(spi_slave_transaction_t *trans)
+{
+  digitalWrite(SPI_HS, HIGH);
+}
+
+static SPIClass SPI;
+
+// Replace with your network credentials
+const char* ssid     = "esp32";
+const char* password = "esp32moda";
+
+// Set web server port number to 80
+WiFiServer server (80);
+
+void buildInfoHTML(WiFiClient& client);
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+  Serial.println("IP address: ");
+  Serial.println(WiFi.softAPIP());
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    String header = "";
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            /* turns the GPIOs on and off
+            if (header.indexOf("GET /26/on") >= 0) {
+              Serial.println("GPIO 26 on");
+              output26State = "on";
+              digitalWrite(output26, HIGH);
+            } else if (header.indexOf("GET /26/off") >= 0) {
+              Serial.println("GPIO 26 off");
+              output26State = "off";
+              digitalWrite(output26, LOW);
+            } else if (header.indexOf("GET /27/on") >= 0) {
+              Serial.println("GPIO 27 on");
+              output27State = "on";
+              digitalWrite(output27, HIGH);
+            } else if (header.indexOf("GET /27/off") >= 0) {
+              Serial.println("GPIO 27 off");
+              output27State = "off";
+              digitalWrite(output27, LOW);
+            }*/
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<body><h1>ESP32 Web Server!!</h1>");
+            buildInfoHTML(client);
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
+
+void buildInfoHTML(WiFiClient& client)
+{
+  SPI.begin();
+  client.println("<u>");
+  client.println(SPI.transfer("hello", 6));
+  //client.println("</b><br><u>");
+  //client.println(SPI.transfer("B    ", 6));
+  client.println("</u>");
+  SPI.end();
+}
+
